9177fb1ed37f57b3a79ae63b4f698b7abbccd993
danez13 <hernandezdaniel1702@gmail.com>
2025-08-25 10:08:56 -0400

    added fighters dataset

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -4,105 +4,132 @@ import logging
 import tempfile
 logger = logging.getLogger(__name__)
 class Dataset():
-    def __init__(self, file: str, update:bool, columns: list = ["id"]):
-        self.file = file
-        self.update = update
+    def __init__(self, file: str, update:bool, columns: list = ["id"],disabled:bool=False):
+        self.disabled = disabled
+        if not disabled:
+            self.file = file
+            self.update = update
 
 
-        if os.path.exists(self.file+".csv"):
-            logger.debug(f"Loading data from {self.file}")
-            self.data = pd.read_csv(self.file+".csv")
-            self.columns = self.data.columns.tolist()
-        else:
-            self.columns = columns
-            logger.info(f"File {self.file} does not exist. Initializing new DataFrame.")
-            self.data = pd.DataFrame(columns=self.columns)
+            if os.path.exists(self.file+".csv"):
+                logger.debug(f"Loading data from {self.file}")
+                self.data = pd.read_csv(self.file+".csv")
+                self.columns = self.data.columns.tolist()
+            else:
+                self.columns = columns
+                logger.info(f"File {self.file} does not exist. Initializing new DataFrame.")
+                self.data = pd.DataFrame(columns=self.columns)
 
-        self.tmp_file = tempfile.NamedTemporaryFile(
-                mode='w+', 
-                suffix='.csv', 
-                prefix=f'{self.file}_progress_', 
-                dir="",
-                delete=False
-            )
+            self.tmp_file = tempfile.NamedTemporaryFile(
+                    mode='w+', 
+                    suffix='.csv', 
+                    prefix=f'{self.file}_progress_', 
+                    dir="",
+                    delete=False
+                )
         
     def __getitem__(self,key:str):
-        return self.data[key]
+        if not self.disabled:
+            return self.data[key]
+        else:
+            raise TypeError("dataset is disabled")
 
     def does_id_exist(self, id: str) -> bool:
         """Check if an ID exists in the dataset."""
-        if 'id' not in self.data.columns:
-            raise ValueError("Dataset does not contain 'id' column.")
-                
-        if self.update:
-            return False
-        return id in self.data['id'].values
+
+        if not self.disabled:
+            if 'id' not in self.data.columns:
+                raise ValueError("Dataset does not contain 'id' column.")
+                    
+            if self.update:
+                return False
+            return id in self.data['id'].values
+        else:
+            raise TypeError("dataset is disabled")
     
     def add_row(self, row:dict,prepend:bool=False):
         """Add a new row to the dataset."""
-        if not isinstance(row, dict):
-            raise ValueError("Row must be a dictionary.")
-        
-        new_data = pd.DataFrame([row])
 
-        if self.data.empty:
-            self.data = new_data
-        elif prepend:
-            self.data = pd.concat([new_data, self.data], ignore_index=True)
+        if not self.disabled:
+            if not isinstance(row, dict):
+                raise ValueError("Row must be a dictionary.")
+            
+            new_data = pd.DataFrame([row])
+
+            if self.data.empty:
+                self.data = new_data
+            elif prepend:
+                self.data = pd.concat([new_data, self.data], ignore_index=True)
+            else:
+                self.data = pd.concat([self.data, new_data], ignore_index=True)
         else:
-            self.data = pd.concat([self.data, new_data], ignore_index=True)
+            raise TypeError("dataset is disabled")
 
     def add_rows(self,rows:list[dict],prepend:bool=False):
-        if not isinstance(rows,list):
-            raise ValueError("Rows must be a list")
-        
-        for row in rows:
-            self.add_row(row,prepend=prepend)
+        if not self.disabled:
+            if not isinstance(rows,list):
+                raise ValueError("Rows must be a list")
+            
+            for row in rows:
+                self.add_row(row,prepend=prepend)
+        else: 
+            raise TypeError("dataset is disabled")
 
 
     def update_row(self, id: str, row: dict):
         """Update an existing row in the dataset."""
-        if 'id' not in self.data.columns:
-            raise ValueError("Dataset does not contain 'id' column.")
-        
-        if id not in self.data['id'].values:
-            raise ValueError(f"ID {id} does not exist in the dataset.")
-        
-        # Ensure only valid columns are updated
-        invalid_keys = set(row.keys()) - set(self.data.columns)
-        if invalid_keys:
-            raise ValueError(f"Invalid column(s): {invalid_keys}")
-        
-        self.data.loc[self.data['id'] == id, list(row.keys())] = list(row.values())
+
+        if not self.disabled:
+            if 'id' not in self.data.columns:
+                raise ValueError("Dataset does not contain 'id' column.")
+            
+            if id not in self.data['id'].values:
+                raise ValueError(f"ID {id} does not exist in the dataset.")
+            
+            # Ensure only valid columns are updated
+            invalid_keys = set(row.keys()) - set(self.data.columns)
+            if invalid_keys:
+                raise ValueError(f"Invalid column(s): {invalid_keys}")
+            
+            self.data.loc[self.data['id'] == id, list(row.keys())] = list(row.values())
+        else:
+            raise TypeError("dataset is disabled")
 
 
 
     def update_rows(self, ids: list[str], rows: list[dict]):
-        """Update multiple rows in the dataset."""
-        for id, row in zip(ids, rows):
-            self.update_row(id, row)
+        if not self.disabled:
+            """Update multiple rows in the dataset."""
+            for id, row in zip(ids, rows):
+                self.update_row(id, row)
+        else:
+            raise TypeError("dataset is disabled")
 
 
     def save(self,direct:bool):
         """Save the dataset to the CSV file."""
-        if direct:
-            if not self.file:
-                raise ValueError("File path is not set.")
+        if not self.disabled:
 
-            logger.debug(f"Saving dataset to {self.file}")
-            if os.path.exists(self.tmp_file.name):
-                try:
-                    self.data = pd.read_csv(self.tmp_file.name)
-                except:
-                    pass
-                self.tmp_file.close()
-                os.remove(self.tmp_file.name)
+            if direct:
+                if not self.file:
+                    raise ValueError("File path is not set.")
 
-            self.data.to_csv(self.file+".csv", index=False)
+                logger.debug(f"Saving dataset to {self.file}")
+                if os.path.exists(self.tmp_file.name):
+                    try:
+                        self.data = pd.read_csv(self.tmp_file.name)
+                    except:
+                        pass
+                    self.tmp_file.close()
+                    os.remove(self.tmp_file.name)
 
+                self.data.to_csv(self.file+".csv", index=False)
+
+            else:
+                if not self.tmp_file:
+                    raise ValueError("Temporary file is not set.")
+
+                self.data.to_csv(self.tmp_file.name, index=False)
+                logger.debug(f"Saving dataset to temporary file {self.tmp_file.name}")
         else:
-            if not self.tmp_file:
-                raise ValueError("Temporary file is not set.")
-
-            self.data.to_csv(self.tmp_file.name, index=False)
-            logger.debug(f"Saving dataset to temporary file {self.tmp_file.name}")
+            raise TypeError("dataset is disabled")
--- main.py
+++ main.py
@@ -25,6 +25,21 @@ def attempt_func(func:Callable, args, ignore_errors: bool):
         else:
             raise
 
+############
+# Fighters #
+############
+
+def fighter_listing_scraping(scraper:UFCStatsScraper,char:str,page:int):
+    return scraper.run(scraper.scraper_fighter_listing,parameters={"char":char,"page":page})
+
+def fighter_scraping(scraper:UFCStatsScraper,ids:list,early_stopping:Callable):
+    return scraper.run(
+        scraper.scrape_fighters,
+        parameters={"ids":ids,"early_stopping":early_stopping}
+    )
+##########
+# Fights #
+##########
 
 def fight_scraping(scraper: UFCStatsScraper, events: list[dict], early_stopping):
     data_collection = []
@@ -42,6 +57,10 @@ def fight_scraping(scraper: UFCStatsScraper, events: list[dict], early_stopping)
         data_collection.extend(fights)
     return data_collection
 
+##########
+# Events #
+##########
+
 def event_scraping(scraper:UFCStatsScraper,ids:list[str],early_stopping:Callable):
     return scraper.run(
                 scraper.scrape_events,
@@ -51,6 +70,9 @@ def event_scraping(scraper:UFCStatsScraper,ids:list[str],early_stopping:Callable
 def event_listing_scraping(scraper:UFCStatsScraper,page:int):
     return scraper.run(scraper.scrape_event_listing,{"page":page})
 
+########
+# Main #
+########
 
 def main(cli_args=None, log=True) -> int:
     if log:
@@ -66,59 +88,88 @@ def main(cli_args=None, log=True) -> int:
     parser.add_argument("-u", "--update", 
                         action="store_true", 
                         help="Update previously scraped data")
-    parser.add_argument("-e", "--events-only", 
+    parser.add_argument("-e", "--no-events", 
                         action="store_true", 
-                        help="Scrape only events, not fights")
-    parser.add_argument("-f", "--fights-only", 
+                        help="Do not scraper events")
+    parser.add_argument("-f", "--no-fights", 
                         action="store_true", 
-                        help="Scrape only fights, not events")
+                        help="Do not scrape fights")
+    parser.add_argument("-fi", "--no-fighters", 
+                        action="store_true", 
+                        help="Do not scrape fighters")
     parser.add_argument("-p","--prepend",
                         action="store_true",
                         help="Save intermediate results to temporary files during scraping")
+    parser.add_argument("-w","--wait",
+                        default=10,
+                        type=int,
+                        help="set wait time")
     args = parser.parse_args(cli_args)
 
-    scraper = UFCStatsScraper(wait_time=3, ignore_errors=args.ignore)
+    scraper = UFCStatsScraper(wait_time=args.wait, ignore_errors=args.ignore)
 
     # Initialize datasets
-    events_dataset = Dataset("events", args.update)
-    fights_dataset = Dataset("fights", args.update)
+    events_dataset = Dataset("events", args.update, disabled = True if args.no_events else False)
+    fights_dataset = Dataset("fights", args.update,disabled=True if args.no_fights else False)
+    fighters_dataset = Dataset("fighters", args.update,disabled=True if args.no_fighters else False)
+    fight_fighters_dataset = Dataset("fighter_fights",args.update,disabled=True if args.no_fighters and args.no_fights else False)
 
-    page = 1
-    while True:
-        # --- Scrape event listing page ---
-        event_ids = attempt_func(event_listing_scraping,{"scraper":scraper,"page":page},args.ignore)
+    if not args.no_fights or not args.no_events:
+        page = 1
+        while True:
+            # --- Scrape event listing page ---
+            event_ids = attempt_func(event_listing_scraping,{"scraper":scraper,"page":page},args.ignore)
 
-        events_page_data = []
-        if not args.fights_only:
-            # --- Scrape events for this page ---
-            events_page_data = attempt_func(event_scraping,{"scraper":scraper,"ids":event_ids,"early_stopping":events_dataset.does_id_exist},args.ignore)
-            events_dataset.add_rows(events_page_data,prepend=args.prepend)
-            if len(events_page_data) == 0:
-                logger.info(f"No more events found after page {page}.")
+            events_page_data = []
+            if not args.no_events:
+                # --- Scrape events for this page ---
+                events_page_data = attempt_func(event_scraping,{"scraper":scraper,"ids":event_ids,"early_stopping":events_dataset.does_id_exist},args.ignore)
+                events_dataset.add_rows(events_page_data,prepend=args.prepend)
+                if len(events_page_data) == 0:
+                    logger.info(f"No more events found after page {page}.")
+                    break
+                events_dataset.save(direct=args.direct)
+                logger.info(f"Scraped page {page} with {len(events_page_data)} events")
+
+            # --- Scrape fights for this page ---
+            if not args.no_fights:
+                if args.no_events:
+                    events_page_data = attempt_func(event_scraping,{"scraper":scraper,"ids":event_ids,"early_stopping": lambda x: False},args.ignore)
+                    # For fights-only mode, we need to get fights per event directly
+                fights_page_data = attempt_func(fight_scraping,{"scraper":scraper,"events":events_page_data,"early_stopping":fights_dataset.does_id_exist},args.ignore)
+                if len(fights_page_data) == 0:
+                    logger.info(f"No more fighters found.")
+                    break
+                fights_dataset.add_rows(fights_page_data,prepend=args.prepend)
+                fights_dataset.save(direct=args.direct)
+                logger.info(f"Scraped {len(fights_page_data)} fights from page {page}")
+
+            page += 1
+
+    if not args.no_fighters:
+        char=97
+        while True:
+            page=1
+            while True:
+                fighter_ids = attempt_func(fighter_listing_scraping, {"scraper":scraper,"char":chr(char),"page":page},args.ignore)
+                fighters_page_data = attempt_func(fighter_scraping,{"scraper":scraper,"ids":fighter_ids,"early_stopping":fighters_dataset.does_id_exist},args.ignore)
+                if len(fighters_page_data) == 0:
+                    logger.info(f"No more fights found.")
+                    break
+                fighters_dataset.add_rows(fighters_page_data,prepend=args.prepend)
+                fighters_dataset.save(direct=args.direct)
+                page+=1
+            if char == 122:
                 break
-            events_dataset.save(direct=args.direct)
-            logger.info(f"Scraped page {page} with {len(events_page_data)} events")
-
-        # --- Scrape fights for this page ---
-        if not args.events_only:
-            if args.fights_only:
-                events_page_data = attempt_func(event_scraping,{"scraper":scraper,"ids":event_ids,"early_stopping": lambda x: False},args.ignore)
-                # For fights-only mode, we need to get fights per event directly
-            fights_page_data = attempt_func(fight_scraping,{"scraper":scraper,"events":events_page_data,"early_stopping":fights_dataset.does_id_exist},args.ignore)
-            if len(fights_page_data) == 0:
-                logger.info(f"No more fights found.")
-                break
-            fights_dataset.add_rows(fights_page_data,prepend=args.prepend)
-            fights_dataset.save(direct=args.direct)
-            logger.info(f"Scraped {len(fights_page_data)} fights from page {page}")
-
-        page += 1
+            char+=1
 
     # --- Final save to proper CSVs ---
-    if not args.fights_only:
+    if not args.no_fights:
         events_dataset.save(direct=True)
-    if not args.events_only:
+    if not args.no_events:
         fights_dataset.save(direct=True)
+    if not args.no_fighters:
+        fighters_dataset.save(direct=True)
 
     return 0
 
